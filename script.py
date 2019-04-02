0fc6a92d3e4265d2c66157ba5e96d9298a0b4bc6
juanparroAx <46005913+juanparroAx@users.noreply.github.com>
2019-04-02 10:54:01 -0500

    Update

--- script.py
+++ script.py
@@ -1,2 +1,165 @@
-while True:
-	print("I am working!")
+    # -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, time
+from pprint import pprint
+import requests
+import json
+import sys
+import os
+try:
+    from flask import Flask
+    from flask import request
+except ImportError as e:
+    print(e)
+    print("Looks like 'flask' library is missing.\n"
+          "Type 'pip3 install flask' command to install the missing library.")
+    sys.exit()
+
+
+bearer = "YzVkMjcxMDQtYjU1MC00NTM0LTk1Y2UtYjRkNTVkZjE0MjdjOGQ5YzViNTgtZmNi" # BOT'S ACCESS TOKEN
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Bearer " + str(bearer)
+}
+
+def send_get(url, payload=None,js=True):
+
+    if payload == None:
+        request = requests.get(url, headers=headers)
+    else:
+        request = requests.get(url, headers=headers, params=payload)
+    if js == True:
+        request= request.json()
+    return request
+
+
+def send_post(url, data):
+
+    request = requests.post(url, json.dumps(data), headers=headers).json()
+    return request
+
+def pretty_print (content):
+# Note: We convert content to and from an object so we can pretty-print it.
+    return json.loads(content)
+
+def get_answers (host, endpoint_key, path, content):
+    print ('Calling ' + host + path + '.')
+    headers = {
+        'Authorization': 'EndpointKey ' + endpoint_key,
+        'Content-Type': 'application/json',
+        'Content-Length': len(content)
+    }
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("POST", path, content, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+def qnaMaker(query):
+
+  # **********************************************
+  # *** Update or verify the following values. ***
+  # **********************************************
+  returnRta = ""
+  # NOTE: Replace this with a valid host name.
+  host = "samiqnaproduction.azurewebsites.net"
+
+  # NOTE: Replace this with a valid endpoint key.
+  # This is not your subscription key.
+  # To get your endpoint keys, call the GET /endpointkeys method.
+  endpoint_key = "1c0be32e-5325-4a93-bd83-2d8d5d5e71ba"
+
+  # NOTE: Replace this with a valid knowledge base ID.
+  # Make sure you have published the knowledge base with the
+  # POST /knowledgebases/{knowledge base ID} method.
+  kb = "e27c2cfa-0df7-43b6-a10a-e55e284fd25d"
+
+  method = "/qnamaker/knowledgebases/" + kb + "/generateAnswer"
+
+  question = {
+      'question': query
+  }
+
+   # Convert the request to a string.
+  content = json.dumps(question)
+  result = get_answers (host, endpoint_key, method, content)
+
+  final = pretty_print(result)
+  returnRta = final['answers'][0]['answer']
+
+  return returnRta
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def teams_webhook():
+    if request.method == 'POST':
+        webhook = request.get_json(silent=True)
+        if webhook['data']['personEmail']!= bot_email:
+            pprint(webhook)
+        if webhook['resource'] == "memberships" and webhook['data']['personEmail'] == bot_email:
+            send_post("https://api.ciscospark.com/v1/messages",
+                            {
+                                "roomId": webhook['data']['roomId'],
+                                "markdown": (greetings() +
+                                             "**Note This is a group room and you have to call "
+                                             "me specifically with `@%s` for me to respond**" % bot_name)
+                            }
+                            )
+        msg = None
+        if "@webex.bot" not in webhook['data']['personEmail']:
+            result = send_get(
+                'https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
+            in_message = result.get('text', '').lower()
+            in_message = in_message.replace(bot_name.lower() + " ", '')
+
+            # Aqui tiene que entrar la API de SAMI
+
+            if in_message != "":
+                msg = qnaMaker(in_message)
+            else:
+                msg = "Sorry, but I did not understand your request. Type `Help me` to see what I can do"
+            if msg != None:
+                send_post("https://api.ciscospark.com/v1/messages",
+                                {"roomId": webhook['data']['roomId'], "markdown": msg})
+        return "true"
+    elif request.method == 'GET':
+        message = "<center><img src=\"https://cdn-images-1.medium.com/max/800/1*wrYQF1qZ3GePyrVn-Sp0UQ.png\" alt=\"Webex Teams Bot\" style=\"width:256; height:256;\"</center>" \
+                  "<center><h2><b>Congratulations! Your <i style=\"color:#ff8000;\">%s</i> bot is up and running.</b></h2></center>" \
+                  "<center><b><i>Don't forget to create Webhooks to start receiving events from Webex Teams!</i></b></center>" % bot_name
+        return message
+
+def main():
+    global bot_email, bot_name
+    if len(bearer) != 0:
+        test_auth = send_get("https://api.ciscospark.com/v1/people/me", js=False)
+        if test_auth.status_code == 401:
+            print("Looks like the provided access token is not correct.\n"
+                  "Please review it and make sure it belongs to your bot account.\n"
+                  "Do not worry if you have lost the access token. "
+                  "You can always go to https://developer.webex.com/my-apps "
+                  "and generate a new access token.")
+            sys.exit()
+        if test_auth.status_code == 200:
+            test_auth = test_auth.json()
+            bot_name = test_auth.get("displayName","")
+            bot_email = test_auth.get("emails","")[0]
+    else:
+        print("'bearer' variable is empty! \n"
+              "Please populate it with bot's access token and run the script again.\n"
+              "Do not worry if you have lost the access token. "
+              "You can always go to https://developer.webex.com/my-apps "
+              "and generate a new access token.")
+        sys.exit()
+
+    if "@webex.bot" not in bot_email:
+        print("You have provided an access token which does not relate to a Bot Account.\n"
+              "Please change for a Bot Account access token, view it and make sure it belongs to your bot account.\n"
+              "Do not worry if you have lost the access token. "
+              "You can always go to https://developer.webex.com/my-apps "
+              "and generate a new access token for your Bot.")
+        sys.exit()
+    else:
+        app.run(host='localhost', port=8080)
+
+if __name__ == "__main__":
+    main()
